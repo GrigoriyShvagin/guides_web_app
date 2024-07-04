34ca769f3e96684ebdb7b237b5fbbd492135256f
geteroseksuAll <Shvagin04@mail.ru>
2024-07-04 22:22:12 +0300

    lil changes

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { computed } from "vue";
 import AppLayout from "./Layouts/AppLayout.vue";
 defineComponent({ name: "App" });
 
-Telegram.WebApp.setBackgroundColor("#0d0d0d");
+window.Telegram.WebApp.setBackgroundColor("#0d0d0d");
 const layout = computed(() => AppLayout);
 </script>
 
