cc67cf9a722cd6cba827e0732e22492364073d19
geteroseksuAll <Shvagin04@mail.ru>
2024-07-06 12:52:35 +0300

    test bg

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@ defineComponent({ name: "App" });
 
 window.Telegram.WebApp.setHeaderColor("#0d0d0d");
 window.Telegram.WebApp.setBackgroundColor("#0d0d0d");
+window.Telegram.WebApp.themeParams = {
+  section_bg_color: "#0d0d0d",
+  secondary_bg_color: "#0d0d0d",
+};
 
 const layout = computed(() => AppLayout);
 </script>
